a51ef3797a22441aa3a826cc120af52abac94fa8
Larissa Souza <souza.larissa@aluno.ifsp.edu.br>
2022-06-23 20:29:33 -0300

    ✨Adicionando estilo aos Exercícios Condicionais

--- exerciciosCondicionais/exercicio1.php
+++ exerciciosCondicionais/exercicio1.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Exercícios - 1 </title>
+  
+  <style type="text/css">
+    * {
+        font-family: 'Lato', sans-serif;
+    }
+    </style>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
   <?php
   function  receiveNumber(){
@@ -25,4 +37,5 @@
   ?>
   <?php receiveNumber(); ?>
 </body>
+
 </html>
--- exerciciosCondicionais/exercicio2.php
+++ exerciciosCondicionais/exercicio2.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Exercícios - 2</title>
+
+  <style type="text/css">
+    * {
+        font-family: 'Lato', sans-serif;
+    }
+    </style>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
   <?php
   function  receiveValues(){
@@ -20,4 +32,5 @@
   ?>
   <?php receiveValues(); ?>
 </body>
+
 </html>
